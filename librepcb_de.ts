fcf237767848ffc995ff161add256d806f0ed13a
LibrePCB Builder <builder@librepcb.org>
2019-03-13 21:26:22 +0000

    Update translations for 4475d331
    2019-03-13T20:38:27+01:00

--- librepcb_de.ts
+++ librepcb_de.ts
@@ -4927,12 +4927,12 @@ Are you still sure to delete the following library elements?</source>
         <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../libs/librepcb/libraryeditor/dev/padsignalmapeditorwidget.cpp" line="236"/>
+        <location filename="../libs/librepcb/libraryeditor/dev/padsignalmapeditorwidget.cpp" line="245"/>
         <source>(not connected)</source>
         <translation>(nicht verbunden</translation>
     </message>
     <message>
-        <location filename="../libs/librepcb/libraryeditor/dev/padsignalmapeditorwidget.cpp" line="263"/>
+        <location filename="../libs/librepcb/libraryeditor/dev/padsignalmapeditorwidget.cpp" line="269"/>
         <source>Could not set signal</source>
         <translation type="unfinished"/>
     </message>
