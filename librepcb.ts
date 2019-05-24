e814219c2f60a04d8e19b1d94a70d16a73086d29
LibrePCB Builder <builder@librepcb.org>
2019-05-24 15:51:31 +0000

    Update translations for a7dffaba
    2019-05-24T17:40:30+02:00

--- librepcb.ts
+++ librepcb.ts
@@ -5420,7 +5420,7 @@ Are you still sure to delete the following library elements?</translation>
         <translation>Rotation:</translation>
     </message>
     <message>
-        <location filename="../libs/librepcb/libraryeditor/sym/dialogs/symbolpinpropertiesdialog.cpp" line="99"/>
+        <location filename="../libs/librepcb/libraryeditor/sym/dialogs/symbolpinpropertiesdialog.cpp" line="102"/>
         <source>Error</source>
         <translation>Error</translation>
     </message>
